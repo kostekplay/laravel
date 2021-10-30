38d454d685e03de9a0d6dc86c50d89f62b4d5e29
Jacek Kostowski <jacekkostowski@gmail.com>
2021-10-30 22:50:16 +0200

    session flash messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,10 @@
 </head>
 <body>
     <div>
+        @if (session('status'))
+            <div style="background: red;color:white">{{ session('status') }}</div>
+        @endif
+
         @yield('content')
     </div>
 </body>
