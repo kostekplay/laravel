77511453fd46108c89eaaf36abc3782f091e9e3b
Jacek Kostowski <jacekkostowski@gmail.com>
2021-11-09 17:00:10 +0100

    Instaling Bootstrap CSS Framework

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,7 +11,6 @@ class PostsController extends Controller
     public function index()
     {
         //return view('posts.index', ['post' => BlogPost::orderBy('created_at','desc')->take(5)->get()]);
-        //return view('posts.index', ['post' => BlogPost::all()]);
         return view('posts.index', ['posts' => BlogPost::all()]);
     }
 
