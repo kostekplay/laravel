6659ecc30fbbcaa061776a76c0b3973c1e7d8f1b
Jacek Kostowski <jacekkostowski@gmail.com>
2021-11-12 15:39:49 +0100

    Styling Forms

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,8 +1,14 @@
-<div><input type="text" name="title" value="{{ old('title', optional($post ?? null)->title) }}"></div>
+<div class="form-group">
+    <label for="title">Title</label>
+    <input class="form-control" type="text" name="title" id="title" value="{{ old('title', optional($post ?? null)->title) }}">
+</div>
 @error('title')
-<div>{{ $message }}</div>
+    <div>{{ $message }}</div>
 @enderror
-<div><textarea name="content">{{ old('content', optional($post ?? null)->content) }}</textarea></div>
+<div class="form-group">
+    <label for="content">Content</label>
+    <textarea class="form-control" id="content" name="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
+</div>
 @if ($errors->any())
     <div>
         <ul>
